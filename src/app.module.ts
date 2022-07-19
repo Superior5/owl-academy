d53357580e561109ed8e5e713b109893f185dd02
Superior5 <superiorunknown5@gmail.com>
2022-07-20 00:35:43 +0300

    Course & Lesson CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CourseModule } from './course/course.module';
+import { LessonModule } from './lesson/lesson.module';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [],
+  imports: [CourseModule, LessonModule],
   controllers: [AppController],
   providers: [AppService],
 })
